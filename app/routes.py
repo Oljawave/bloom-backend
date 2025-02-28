dc94d92f24b1556a105ef3e181986ea66a450ba1
Oljawave <misterolzhas04@gmail.com>
2025-03-01 04:51:43 +0600

    dates new

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,19 @@
-from flask import jsonify, request
-from app import app, supabase
+from flask import jsonify, request, Response
 import json
+from app import app, supabase
 
 @app.route('/orders', methods=['POST'])
 def create_order():
     try:
         data = request.json
-
-
-        if isinstance(data.get("selected_dates"), list):
-            data["selected_dates"] = json.dumps(data["selected_dates"])
-
         response = supabase.table("orders").insert(data).execute()
 
         if response.data:
-            return jsonify({"message": "Order created!", "order": response.data}), 201
+            return Response(
+                json.dumps({"message": "Order created!", "order": response.data}, ensure_ascii=False),
+                status=201,
+                mimetype="application/json"
+            )
         return jsonify({"error": "Failed to create order"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
